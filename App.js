e3dcc6b3302d16b3a0781a5de2cffe274a426140
ouyanglinxia <ouyanglinxia@xiaomi.com>
2020-03-30 14:51:25 +0800

    init

---
+++ App.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>React-Native入门学习</Text>
+        <Image
+          style={styles.pic}
+          source={{
+            uri: 'https://avatars3.githubusercontent.com/u/6133685?v=3&s=460',
+          }}
+        />
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: 'red',
+  },
+  pic: {
+    width: 100,
+    height: 100,
+  },
+});
+
+export default App;
